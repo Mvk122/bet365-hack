ba158ac4f1419dd3d3e530073c29208a722d8247
Madhav Khandhar <madhav.khandhar@student.manchester.ac.uk>
2023-03-01 13:18:56 +0000

    Added table creation

--- db.py
+++ db.py
@@ -6,11 +6,20 @@ db_user_password = "bet365admin"
 
 def create_connection(db_name, db_user, db_user_password):
     connection = psycopg2.connect(database=db_name, user=db_user, password=db_user_password)
-    return connection.cursor()
+    return connection
 
-def create_tables(cursor):
-    user_table = "CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT NOT NULL)"
+def create_tables(connection):
+    user_table = "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT NOT NULL)"
+    item_table = "CREATE TABLE IF NOT EXISTS items (id SERIAL PRIMARY KEY, name TEXT NOT NULL)"
+    order_table = "CREATE TABLE IF NOT EXISTS orders (id SERIAL PRIMARY KEY, user_id INT, item_id INT, CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items(id), CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id))"
+
+    cursor = connection.cursor()
+    
     cursor.execute(user_table)
+    cursor.execute(item_table)
+    cursor.execute(order_table)
+    connection.commit()
+
 
 if __name__ == "__main__":
     connection = create_connection(db_name, db_user, db_user_password)
