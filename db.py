134d5991c3278bb2017786a931bee86088fd112b
Madhav Khandhar <madhav.khandhar@student.manchester.ac.uk>
2023-03-01 14:07:09 +0000

    completed naive solution

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ def create_connection(db_name, db_user, db_user_password):
 def create_tables(connection):
     user_table = "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT NOT NULL)"
     item_table = "CREATE TABLE IF NOT EXISTS items (id SERIAL PRIMARY KEY, name TEXT NOT NULL)"
-    order_table = "CREATE TABLE IF NOT EXISTS orders (id SERIAL PRIMARY KEY, user_id INT, item_id INT, CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items(id), CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id))"
+    order_table = "CREATE TABLE IF NOT EXISTS orders (id SERIAL PRIMARY KEY, user_id INT, item_id INT, order_date DATE, CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items(id), CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id))"
 
     cursor = connection.cursor()
 
--- naive.py
+++ naive.py
@@ -5,7 +5,7 @@ def get_user_id(user, connection):
     cursor = connection.cursor()
     cursor.execute(f"select id from users where name='{user}'")
     record = cursor.fetchall()
-    if len(record == 0):
+    if len(record) ==0:
         return False
     else:
         return record[0][0]
@@ -15,7 +15,7 @@ def get_item_id(item, connection):
     cursor = connection.cursor()
     cursor.execute(f"select id from items where name='{item}'")
     record = cursor.fetchall()
-    if len(record == 0):
+    if len(record) == 0:
         return False
     else:
         return record[0][0]
@@ -30,18 +30,27 @@ def create_item(item, connection):
     connection.commit()
     return get_item_id(item, connection)
 
-def create_transaction(item_id, user_id, connection):
-    connection.cursor().execute(f"INSERT INTO orders(user_id, item_id) VALUES ({user_id}, {item_id})")
+def create_transaction(item_id, user_id, order_date, connection):
+    connection.cursor().execute(f"INSERT INTO orders(user_id, item_id, order_date) VALUES ({user_id}, {item_id}, '{order_date}')")
     connection.commit()
+
 
 def insert_row(user, item, date, connection):
     user_id = get_user_id(user, connection)
     if type(user_id) != int:
-        user_id = create_user(user)
+        user_id = create_user(user, connection)
     
     item_id = get_item_id(item, connection)
     if type(item_id) != int:
-        item_id = create_item(item)
+        item_id = create_item(item, connection)
+
+    create_transaction(item_id, user_id, date, connection)
+
+with open("customer_orders.csv", "r") as f:
+    for line in f:
+        user, item, date = line.split(",")
+        insert_row(user[1:-1], item[1:-1], date[1:-1], db.default_connection())
+
 
     
 
