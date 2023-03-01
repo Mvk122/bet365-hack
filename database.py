5ce21d10a78fa9927ba7956db2966cf1a019d9fa
Nicolo Micheletti <nicolo.micheletti@student.manchester.ac.uk>
2023-03-01 16:19:29 +0000

    Mongo DB script

--- database.py
+++ database.py
@@ -7,17 +7,28 @@ class MongoDatabase:
       self.db = self.get_database(url)
 
    def get_database(self, url):
-        CONNECTION_STRING = url
-        client = MongoClient(CONNECTION_STRING)
+      CONNECTION_STRING = url
+      client = MongoClient(CONNECTION_STRING)
         
-        return client['Bet365']
+      return client['Bet365']
 
    def create_collection(self):
-        self.transactions = self.db["transactions"]
+      self.transactions = self.db["transactions"]
 
-   def insert_transaction(self, transaction_url):
-
+   def insert_transaction(self, transaction)
+      self.transactions.insert_many(transaction)
 
 if __name__ == "__main__":   
-    db = MongoDatabase(creds.db_url)
-    db.insert_transaction()
+   db = MongoDatabase(creds.db_url)
+   with open("customer_orders.csv", "r") as f:
+        for line_number, line in enumerate(f):
+            if line_number % 10000 == 0:
+               print("here")           
+            user, item, date = line.split(",")
+            transaction = {
+               "user" : user,
+               "item" : item,
+               "date": date,
+            }
+
+            db.insert_transaction(transaction)
