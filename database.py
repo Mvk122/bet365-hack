8dfc268ca73614eccae9d8cbb5f9964289c1365c
Nicolo Micheletti <nicolo.micheletti@student.manchester.ac.uk>
2023-03-01 16:12:35 +0000

    add mongo script

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from pymongo import MongoClient
 import creds 
 
@@ -32,3 +33,28 @@ if __name__ == "__main__":
             }
 
             db.insert_transaction(transaction)
+=======
+from pymongo import MongoClient
+import creds 
+
+class MongoDatabase:
+
+   def __init__(self, url):
+      self.db = self.get_database(url)
+
+   def get_database(self, url):
+        CONNECTION_STRING = url
+        client = MongoClient(CONNECTION_STRING)
+        
+        return client['Bet365']
+
+   def create_collection(self):
+        self.transactions = self.db["transactions"]
+
+   def insert_transaction(self, transaction_url):
+
+
+if __name__ == "__main__":   
+    db = MongoDatabase(creds.db_url)
+    db.insert_transaction()
+>>>>>>> 4e0b85c (add mongo script)
